77bb10de81128bdb28f5d70f71944d026d5ef081
Chanwoo Ahn <cwahn@arsvivendi.io>
2024-07-03 19:06:50 +0900

    build: Release 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="entoli",
-    version="0.1.3",
+    version="0.1.4",
     author="Chanwoo Ahn",
     author_email="cwahn0904@gmail.com",
     description="A Python functional programming library",
